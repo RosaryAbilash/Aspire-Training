3dba668bfef2d16ccbcafa997e2a372cf9095c93
Rosary Abilash <75167373+RosaryAbilash@users.noreply.github.com>
2024-03-26 18:44:11 +0530

    String Methods

---
+++ Java Script ACTIVITY - 4/Strings.js
@@ -0,0 +1,19 @@
+let myName = "Rosary Abilash"
+
+console.log(myName)
+
+
+// Length
+const nameLength = myName.length
+
+console.log(nameLength)
+
+// charAt
+
+var lastLetter = myName.charAt(nameLength-1)
+
+console.log(lastLetter)
+
+// charCodeAt
+
+console.log("Code of Last Letter ", myName.charCodeAt(nameLength-1))
