837db3d297e6cd7ec78f767ed4a5b74fb80cbacf
RosaryAbilash <rosaryabilash@gmail.com>
2024-04-09 07:12:20 +0530

    New OOPS Avctivity

---
+++ OOPS ACTIVITY - 7/EngineeringCollege.java
@@ -0,0 +1,63 @@
+class College{
+
+    float courseFees = 55000;
+
+
+    String getCollegeName(){
+        return "College";
+    }
+
+    String getAvailableCourse(){
+        return "CSE,ECE,IT";
+    }
+    // void display(){
+    //     System.out.println("College Name " + getCollegeName());
+    //     System.out.println("Courses Available " + getAvailableCourse());
+    //     System.out.println("FEES "+courseFees);
+    // }
+}
+
+public class EngineeringCollege extends College{
+    String name;
+    float courseFees;
+
+    EngineeringCollege(String name){
+        this.name = name;
+        this.courseFees = super.courseFees;
+    }
+
+    String getCollegeName(){
+        return name;
+    }
+
+    float calculateFess(){
+        return courseFees + 25000;
+    }
+
+    String getAvailableCourse(){
+        return "CSE,ECE,IT,MECH,CIVIL,AIML,AIDS";
+    }
+
+    void display(){
+        System.out.println("College Name " + getCollegeName());
+        System.out.println("Courses Available " + getAvailableCourse());
+        System.out.println("FEES "+calculateFess());
+
+        System.err.println("Calling Super Class");
+
+        System.out.println("College Name " + super.getCollegeName());
+        System.out.println("Courses Available " + super.getAvailableCourse());
+        System.out.println("FEES "+super.courseFees);
+
+        // super.display()
+
+    }
+
+    public static void main(String[] args) {
+        EngineeringCollege engineeringCollege = new EngineeringCollege("IFET");
+
+        engineeringCollege.display();
+
+    }
+
+}
